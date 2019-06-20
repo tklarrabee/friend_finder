33af2296fc7f7eb20b2784254c85d65bc0ce11bf
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-06-19 19:17:04 -0700

    schema

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    var friends = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Bhmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            2,
+            3,
+            4,
+            2,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Chmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            2,
+            3,
+            4,
+            2,
+            2,
+            5
+        ]
+    }];
+
+    
+
+
+});
+
+
